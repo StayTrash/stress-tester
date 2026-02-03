f93902e7adb8c40f8a45a7f1b7ac315e65590334
who-shubham <shubham.2022ug2012@iiitranchi.ac.in>
2026-02-04 02:24:42 +0530

    fix worker

--- server/prisma/migrations/20260203190329_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "StressJob" (
-    "id" TEXT NOT NULL,
-    "status" TEXT NOT NULL,
-    "result" JSONB,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "StressJob_pkey" PRIMARY KEY ("id")
-);
